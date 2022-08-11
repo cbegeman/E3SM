b7675702a1912163721e247209ce663ffcbf95e9
Carolyn Begeman <cmbranecky@gmail.com>
2022-08-11 12:21:15 -0700

    Use SMARTREDIS_PATH build_options.mk

--- components/mpas-ocean/src/build_options.mk
+++ components/mpas-ocean/src/build_options.mk
@@ -11,7 +11,7 @@ FCINCLUDES += -I$(ROOT_DIR)/BGC
 FCINCLUDES += -I$(ROOT_DIR)/MARBL/include
 FCINCLUDES += -I$(ROOT_DIR)/gotm/build/modules
 FCINCLUDES += -I$(ROOT_DIR)/ppr/src
-FCINCLUDES += -I$(SMARTREDIS_PATH)/install/include
+FCINCLUDES += -I$(SMARTREDIS_PATH)/include
 override CPPFLAGS += -DCORE_OCEAN
 
 report_builds:
