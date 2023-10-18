784a8ae60bdfd2c1a2c50e909ca85bf09c7153ff
Carolyn Begeman <cbegeman@lanl.gov>
2023-10-18 15:18:15 -0500

    WIP python script

--- components/mpas-ocean/tridiag.py
+++ components/mpas-ocean/tridiag.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def _mpas_tridiag(vertViscTopOfEdge, layerThickEdgeMean, dt, normalVelocity):
 
@@ -36,10 +37,10 @@ def _mpas_tridiag(vertViscTopOfEdge, layerThickEdgeMean, dt, normalVelocity):
 
     print('C')
     print(C)
-    print('bTemp')
-    print(bTemp)
     print('rTemp')
     print(rTemp)
+    #print('bTemp')
+    #print(bTemp)
 
     A = ( -2. * dt * vertViscTopOfEdge[N] /
         (layerThickEdgeMean[N-1] + layerThickEdgeMean[N]) /
@@ -55,18 +56,114 @@ def _mpas_tridiag(vertViscTopOfEdge, layerThickEdgeMean, dt, normalVelocity):
 
     return normalVelocity
 
+def _schopf_tridiag(vertViscTopOfEdge, layerThickEdgeMean, dt, normalVelocity):
+
+    # tridiagonal matrix algorithm currently in mpas-ocean
+    # vertViscTopOfEdge
+    # layerThickEdgeMean
+    # dt
+    # normalVelocity of size layerThickEdgeMean
+    # crop each of the vectors so they range from minLevelEdgeBot to maxLevelEdgeTop
+    print('enter tridiag')
+    Nsurf = 0  # top index
+    N = len(normalVelocity) - 1  # bottom index
+
+    alpha = np.zeros((len(normalVelocity)))
+    A = np.zeros((len(normalVelocity)))
+    B = np.zeros((len(normalVelocity)))
+    C = np.zeros((len(normalVelocity)))
+    D = np.zeros((len(normalVelocity)))
+    E = np.zeros((len(normalVelocity)))
+    F = np.zeros((len(normalVelocity)))
+    h = np.zeros((len(normalVelocity)))
+
+    # Using POP notation now:
+
+    # This time, we don't divide through by layerThickEdgeMean
+    k = Nsurf
+    dz = (layerThickEdgeMean[Nsurf] + layerThickEdgeMean[Nsurf+1]) / 2.
+    h[Nsurf] = layerThickEdgeMean[Nsurf]
+    A[Nsurf] = dt * vertViscTopOfEdge[Nsurf+1] / dz
+    #D[Nsurf] = h[Nsurf] + A[Nsurf] # hfac_u(k) = dz(k)/c2dtu = dz/(2 * dt)
+    alpha[Nsurf] = 0.0
+    D[Nsurf] = h[Nsurf] * normalVelocity[Nsurf]
+    E[Nsurf] = A[Nsurf] / (h[Nsurf] + A[Nsurf]) # alpha should really be at k-1 here
+    # B[Nsurf] = h[Nsurf] * E[Nsurf]
+    # B[Nsurf] = h[Nsurf] * A[Nsurf]
+    #F[Nsurf] = h[Nsurf] * normalVelocity[Nsurf] / h[Nsurf] + D[Nsurf]
+    F[Nsurf] = D[Nsurf] / (h[Nsurf] + A[Nsurf]) # alpha should really be at k-1 here
+
+    # first pass: set the coefficients
+    for k in range(Nsurf+1, N+1):
+        h[k] = layerThickEdgeMean[k]
+        C[k] = A[k-1]
+        A[k] = ( 2. * dt * vertViscTopOfEdge[k] /
+            (layerThickEdgeMean[k-1] + layerThickEdgeMean[k]))
+        D[k] = h[k] * normalVelocity[k]
+        #D[k] = h[k] + A[k] + B[k-1]
+        alpha[k] = A[k]* (h[k]+alpha[k-1]) / (h[k] + A[k] + alpha[k-1])
+        # E[k] = A[k] / D[k]
+        E[k] = A[k] / (h[k] + A[k] + alpha[k-1])
+        # B[k] = E[k] * (h[k] + B[k-1])
+        #F[k] = ( h[k] * normalVelocity[k] + 
+        #         C[k] * F[k-1] ) / D[k]
+        F[k] = ( D[k] + A[k-1]*F[k-1] ) / (h[k] + A[k] + alpha[k-1])
+
+    print('alpha')
+    print(alpha)
+    print('A')
+    print(A)
+    print('B')
+    print(B)
+    print('D')
+    print(D)
+    print('E')
+    print(E)
+    print('F')
+    print(F)
+
+    # second pass: back substitution
+
+    # Treat the bottom index separately
+    # We do not apply bottom drag, unlike mpas
+    Ftemp = ( A[N]*F[N] ) / (alpha[N])
+    # this is F[k+1] but without terms below boundary 
+    normalVelocity[N] = F[N] + E[k] * Ftemp
+
+    for k in range(N-1, Nsurf-1, -1):
+        normalVelocity[k] = F[k] + E[k] * F[k+1]
+
+    return normalVelocity
+
 def main():
-    print('Entering main')
-    dt = 10.
-    nz = 100
-    h = np.ones((nz))
-    nu = np.ones((nz))
-    u_old = np.linspace(1, 0, num=nz)
-    print(u_old)
+    dt = 1.
+    nz = 20
+    h = 2. * np.ones((nz))
+    nu = 2. * np.ones((nz))
+    z = np.linspace(0, nz, num=nz)
+    u = np.linspace(1, 0, num=nz)
+    u_old = u.copy()
     
+    print('MPAS')
+    print('Input:')
+    print(u_old)
     u_new = _mpas_tridiag(nu, h, dt, u_old)
-    print(u_new)
-    # return print(u_new)
+    u_mpas = u_new.copy()
+    print('Output:')
+    print(u_mpas)
+    print('Schopf')
+    print('Input:')
+    print(u)
+    u_mod = _schopf_tridiag(nu, h, dt, u)
+    print('Output:')
+    print(u_mod)
+    fig = plt.figure()
+    plt.plot(u_new - u_old, z, 'k:', label='old')
+    plt.plot(u_mod - u_old, z, 'k:', label='mod')
+    #plt.plot(u_old, z, 'k:', label='u_old')
+    #plt.plot(u_new, z, 'k-', label='u_new')
+    plt.legend()
+    plt.savefig('tridiag.png')
 
 if __name__ == "__main__":
     main()
