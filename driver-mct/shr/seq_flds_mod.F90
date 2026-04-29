232c72cac8b449140cc9469a8062b6702269200d
Carolyn Begeman <cbegeman@lanl.gov>
2026-04-29 13:56:34 -0700

    Add x2o Faox_taux to driver-mct

--- driver-mct/shr/seq_flds_mod.F90
+++ driver-mct/shr/seq_flds_mod.F90
@@ -1323,6 +1323,9 @@ contains
     call seq_flds_add(x2a_fluxes,"Faxx_taux")
     call seq_flds_add(i2x_fluxes,"Fioi_taux")
     call seq_flds_add(x2o_fluxes,"Foxx_taux")
+    call seq_flds_add(x2o_fluxes,"Faox_taux")
+    ! Alternatively, subtract the ice-ocean stress which will already by scaled by iceFraction
+    ! call seq_flds_add(x2o_fluxes,"Fioi_taux")
     longname = 'Zonal surface stress'
     stdname  = 'surface_downward_eastward_stress'
     units    = 'N m-2'
@@ -1346,6 +1349,7 @@ contains
     call seq_flds_add(x2a_fluxes,"Faxx_tauy")
     call seq_flds_add(i2x_fluxes,"Fioi_tauy")
     call seq_flds_add(x2o_fluxes,"Foxx_tauy")
+    call seq_flds_add(x2o_fluxes,"Faox_tauy")
     longname = 'Meridional surface stress'
     stdname  = 'surface_downward_northward_stress'
     units    = 'N m-2'
